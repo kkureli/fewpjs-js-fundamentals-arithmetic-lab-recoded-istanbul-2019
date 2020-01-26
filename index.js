e86a52fac42b9bbbc98e31c8ec891512ac0fbcfa
Kaan <kkureli@gmail.com>
2020-01-26 21:44:33 +0300

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-
 // Write your code here
+let num1 = 2;
+let num2 = 31
+var multiply = num1 * num2
+
+var random = (Math.random()) + 1
+
+let num3 = 10
+let num4 = 6
+
+var mod = 10 % 6
+
+let set = [20, 19, 18, 17]
+
+let max = Math.max(20, 19, 18, 17)
